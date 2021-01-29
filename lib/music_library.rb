47d28c51f28fcf6183a595424eda226c25cf9b22
github username <github email address>
2021-01-29 16:19:20 -0500

    commit tree c11add6909fb206429bf477310c6c5e7f4928cc9

--- lib/music_library.rb
+++ lib/music_library.rb
@@ -20,7 +20,7 @@ class MusicLibraryController
             puts "To play a song, enter 'play song'."
             puts "To quit, type 'exit'."
             puts "What would you like to do?"
-            entry = gets
+            entry = gets.strip
         
             case entry
             when "list songs"
